784b3631245aaec1c0febd5925ad370470698899
Joel <joel@Joelui-MacBookPro.local>
2025-03-10 18:49:38 +0900

    [DATA-001] add. added function for retreating best result of run when the codes pushed

--- common_functions.py
+++ common_functions.py
@@ -1,7 +1,28 @@
 import mlflow
 import mlflow.artifacts
+import pandas as pd
 
-run_id = "dbaa981974b1475bb5ca71595353f735"
 
-artifact_uri = f"/Users/joel/Documents/github/MLOps_test/mlruns/0/{run_id}/artifacts/linear_regression.py"
-mlflow.artifacts.download_artifacts(artifact_uri, dst_path="./modeling")
+def get_best_artifacts():
+
+    run_id = "dbaa981974b1475bb5ca71595353f735"
+
+    artifact_uri = f"/Users/joel/Documents/github/MLOps_test/mlruns/0/{run_id}/artifacts/linear_regression.py"
+    mlflow.artifacts.download_artifacts(artifact_uri, dst_path="./modeling")
+
+
+def get_best_result():
+    # Get experiment details
+
+    run_id = "dbaa981974b1475bb5ca71595353f735"
+    run = mlflow.get_run(run_id)
+    
+    if run is None:
+        print(f"Run '{run_id}' not found.")
+        return None
+
+    metrics = run.data.metrics
+
+    print(f"Metrics for run {run_id}: {metrics}")
+    
+    return metrics
--- modeling/linear_regression.py
+++ modeling/linear_regression.py
@@ -10,10 +10,16 @@ import pandas as pd
 
 import os
 
+import common_functions
+
 running_in_github = os.getenv("GITHUB_ACTIONS") == "true"
 
 if running_in_github:
-    data = pd.read_csv('dvc_storage_S3/files/md5/73/2b44631f0cf1242a668a04d542700a')
+
+    common_functions.get_best_result()
+
+
+
 
 else:
     path = 'pre_processed_1_dvcs/merged_data.csv'
@@ -30,37 +36,37 @@ else:
 
     data = pd.read_csv(data_url, sep=",")
 
-test_period = ['2025-07', '2025-07']
+    test_period = ['2025-07', '2025-07']
 
-# 데이터셋 로드
-X = data.drop(columns = ['Datetime', 'PowerConsumption_Zone1', 'PowerConsumption_Zone2', 'PowerConsumption_Zone3'])
-y = data[['PowerConsumption_Zone3']]
+    # 데이터셋 로드
+    X = data.drop(columns = ['Datetime', 'PowerConsumption_Zone1', 'PowerConsumption_Zone2', 'PowerConsumption_Zone3'])
+    y = data[['PowerConsumption_Zone3']]
 
-# 데이터를 학습 세트와 테스트 세트로 분할
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
+    # 데이터를 학습 세트와 테스트 세트로 분할
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
 
-# run마다의 메타데이터 저장 위치 설정
-mlflow.set_tracking_uri("/Users/joel/Documents/github/MLOps_test/mlruns")
+    # run마다의 메타데이터 저장 위치 설정
+    mlflow.set_tracking_uri("/Users/joel/Documents/github/MLOps_test/mlruns")
 
-# MLflow 실행 시작
-mlflow.start_run()
+    # MLflow 실행 시작
+    mlflow.start_run()
 
-# 모델 학습
-model = LinearRegression()
-model.fit(X_train, y_train)
+    # 모델 학습
+    model = LinearRegression()
+    model.fit(X_train, y_train)
 
-# 예측 및 메트릭 계산
-predictions = model.predict(X_test)
-mse = mean_squared_error(y_test, predictions)
+    # 예측 및 메트릭 계산
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
 
-# 매개변수, 메트릭, 모델 기록
-mlflow.log_param('model_type', 'linear_regression')
-mlflow.log_metric(f"{test_period[0]}_{test_period[1]}", mse)
+    # 매개변수, 메트릭, 모델 기록
+    mlflow.log_param('model_type', 'linear_regression')
+    mlflow.log_metric(f"{test_period[0]}_{test_period[1]}", mse)
 
-# 모델 저장
-mlflow.sklearn.log_model(model, "model")
-mlflow.log_artifact(__file__)
+    # 모델 저장
+    mlflow.sklearn.log_model(model, "model")
+    mlflow.log_artifact(__file__)
 
-# MLflow 실행 종료
-mlflow.end_run()
+    # MLflow 실행 종료
+    mlflow.end_run()
 

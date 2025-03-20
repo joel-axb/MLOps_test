44f6a81b8688694701515bf755708118c66fbebd
Joel <joel@Joelui-MacBookPro.local>
2025-03-21 01:36:30 +0900

    [bj-sku-001] RUN_ID=16fae3fb65254664bd44bd83eb7f950c

--- modeling/commons/get_metrics.py
+++ modeling/commons/get_metrics.py
@@ -2,6 +2,7 @@ import mlflow
 import os
 
 # ✅ Load MLflow Credentials from Environment Variables
+# MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 MLFLOW_TRACKING_USERNAME = os.getenv("MLFLOW_TRACKING_USERNAME")
 MLFLOW_TRACKING_PASSWORD = os.getenv("MLFLOW_TRACKING_PASSWORD")
@@ -9,15 +10,19 @@ MLFLOW_TRACKING_PASSWORD = os.getenv("MLFLOW_TRACKING_PASSWORD")
 # ✅ Read Run ID from GitHub Actions ENV
 run_id = os.getenv("RUN_ID")
 
-if not run_id:
-    raise ValueError("🚨 RUN_ID is not set!")
+# if not run_id:
+#     raise ValueError("🚨 RUN_ID is not set!")
 
 # ✅ Set MLflow Tracking URI
-mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+# mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_tracking_uri("http://15.164.97.14:5000")
+    
+original_value = os.getenv("AWS_PROFILE")
+# os.environ["AWS_PROFILE"] = "axb-dev-general"
 
 # ✅ Fetch run details by run_id
 client = mlflow.tracking.MlflowClient()
-run = client.get_run(run_id)
+run = client.get_run(run_id='16fae3fb65254664bd44bd83eb7f950c')
 
 if not run:
     raise ValueError(f"🚨 No run found for run_id: {run_id}")
@@ -25,6 +30,13 @@ if not run:
 # ✅ Fetch the metric value (modify as needed)
 metric_name = "mape"  # Change this to the metric you need
 metric_value = run.data.metrics.get(metric_name, "N/A")
+
+# 환경 변수 원래 값으로 복구
+if original_value is None:
+    del os.environ["AWS_PROFILE"]  # 원래 없던 값이면 삭제
+else:
+    os.environ["AWS_PROFILE"] = original_value  # 원래 값으로 복구
+
 
 # ✅ Save metric to a file for GitHub Actions
 with open("metric_value.txt", "w") as f:
